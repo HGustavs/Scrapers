eb52262c077e8290b09553acef1e87b22e7c0fcc
Marcus Brohede <a97marbr@gmail.com>
2018-04-11 20:15:17 +0200

    Updated to GM4+ and supports GH as of 2018-04-11

--- GHBlame.js
+++ GHBlame.js
@@ -3,13 +3,13 @@
 // @namespace   blameScraper
 // @description Scraping Blame like a Boss
 // @include     https://github.com/HGustavs/LenaSYS/blame/*
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @version     1
-// @grant       GM_xmlhttpRequest
+// @grant       GM.xmlHttpRequest
 // ==/UserScript==
 function ajaxCall(data) {
   try {
-    GM_xmlhttpRequest({
+    GM.xmlHttpRequest({
       method: 'POST',
       url: 'http://localhost/testwritefile/writefile4.php',
       data: 'str=' + encodeURIComponent(data),
@@ -37,6 +37,7 @@ function writeContent(strr)
   strr = strr.replace(/'/g, '');
   strr = strr.replace(/\s/g, ' ');
   strr = strr.replace(/\s+/g, ' ');
+  strr = strr.replace(/\@/g, '');
   return strr;
 }
 
@@ -172,7 +173,6 @@ var filearr=[
 var blame = '';
 var blamefile = '';
 
-blame += '{';
 
 hr=location.href;
 filename=(hr.slice(hr.lastIndexOf("/")+1));
@@ -183,61 +183,64 @@ path=path.slice(0,path.lastIndexOf("/")+1);
 fileind=filearr.indexOf(filename);
 
 if(fileind>-1){
-blame += '"purl":"' + purl + '",\n';
-blame += '"path":"' + path + '",\n';
-blame += '"filename":"' + filename + '",\n';
-blame += '"fileinfo":"' + writeContent($('.file-info').text()) + '",\n';
+    if(fileind==0){
+        blame += '[';
+    }
+    blame += '{';
+    blame += '"purl":"' + purl + '",\n';
+    blame += '"path":"' + path + '",\n';
+    blame += '"filename":"' + filename + '",\n';
+    blame += '"fileinfo":"' + writeContent($('.file-info').text()) + '",\n';
 
-//file-info-divider
-// file-info
+    //file-info-divider
+    // file-info
 
-blame += '"blames":[';
+    blame += '"blames":[';
 
-var i=0;
-$('.blame-container > .blame-hunk').each(function () {
+    var i=0;
+    $('.blame-container > .blame-hunk').each(function () {
 
-  if(i>0) blame+=",";
-  blame+="{";
+      if(i>0) blame+=",";
+      blame+="{";
 
-  blame+='"blamedate":"'+writeContent($(this).find('.blame-commit-date > time-ago').attr('datetime'))+'",';   
-  blame+='"blameuser":"'+writeContent($(this).find('.blame-commit-avatar').attr('alt'))+'",';
-  blame+='"href":"'+writeContent($(this).find('.blame-commit-title > .message').attr('href'))+'",'; 
-  blame+='"mess":"'+writeContent($(this).find('.blame-commit-title > .message').attr('title'))+'",'; 
-  blame+='"rowcnt":"'+writeContent($(this).find('.blame-commit-info').attr('rowspan')-1)+'",'; 
-  blame +='"rows":[';
-  var ii=0;
-  $(this).find('.blame-line').each(function () {
-    if(ii>0) blame+=",";
-    blame+="{";
-    blame+='"row":"'+writeContent($(this).find('.blame-blob-num').text())+'",'; 
-    blame+='"code":"'+writeContent($(this).find('.blob-code-inner').text())+'"'; 
-    
-    
-    blame+="}";
-    ii++;
-  });
-  blame+="]";  
+      blame+='"blamedate":"'+writeContent($(this).find('.blame-commit-date > time-ago').attr('datetime'))+'",';   
+      blame+='"blameuser":"'+writeContent($(this).find('.avatar > img').attr('alt'))+'",';
+      blame+='"href":"'+writeContent($(this).find('.blame-commit-message > .message').attr('href'))+'",'; 
+      blame+='"mess":"'+writeContent($(this).find('.blame-commit-message > .message').attr('title'))+'",'; 
+      blame +='"rows":[';
+      var ii=0;
+      $(this).find('.width-full > .flex-items-start').each(function () {
+        if(ii>0) blame+=",";
+        blame+="{";
+        blame+='"row":"'+writeContent($(this).find('.blame-blob-num').text())+'",'; 
+        blame+='"code":"'+writeContent($(this).find('.blob-code-inner').text())+'"'; 
+        
+        
+        blame+="}";
+        ii++;
+      });
+      blame+="]";  
+      blame+=',"rowcnt":"'+writeContent(ii)+'"'; 
 
-  blame+="}";
-  i++;
+      blame+="}";
+      i++;
 
-});
+    });
 
 
-// End of blame
-blame+="]\n";
+    // End of blame
+    blame+="]\n";
 
-blame += '},\n';
+    blame += '},\n';
 
-ajaxCall(blame);
+    ajaxCall(blame);
 
-if(fileind+2<filearr.length){
-    location.href=purl+filearr[fileind+3];
-}else{
-  alert("end of scrape "+filename)
-}
+    if(fileind+2<filearr.length){
+        location.href=purl+filearr[fileind+3];
+    }else{
+      alert("end of scrape "+filename)
+    }
   
 }else{
   alert(filename+" not found in array");
 }
-
