a27d38ab05c7b79d2e58e0c7138351b306af4424
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2021-04-16 20:48:34 +0200

    Space and Time rework

--- goGitLog.php
+++ goGitLog.php
@@ -11,6 +11,7 @@ require 'substitution.php';
 
 $commits=Array();
 
+echo "<pre>";
 
 $handle = fopen("../gitlog.txt", "r");
 if ($handle) {
@@ -19,7 +20,7 @@ if ($handle) {
         $i++;
         // if($i>400) break;
 
-        $parents=Array();
+        //$parents=Array();
         $login="";
 
         if(strpos($line,"commit")===0){
@@ -62,17 +63,37 @@ if ($handle) {
     // error opening the file.
 } 
 
+// Space is advanced for each new branch from a specific parent space=parent[space]+parent[children];
+// If only one parent yk=parent[yk] xk=current++;
+
 echo count($commits);
 
-echo "</pre>"
+echo "</pre>";
 
+$i=0;
 echo "<table style='font-family:courier;font-size:12px;' border=1>";
 foreach($commits as $commit){
+
+    if($i++==20) break;
+
+    if($i==0){
+        $commit['space']=0;
+        $commit['time']=0;
+    }else{
+        $commit['space']=1;
+        $commit['time']=$i;
+    }
+
+    $commitid=$commit['commitid'];
+    $parents=$commit['parents'];
+
     echo "<tr>";
     echo "<td>".$commit['commitid']."</td>";
-
+    echo "<td>".$commit['space']."</td>";
+    echo "<td>".$commit['time']."</td>";
     echo "<td>";
-    foreach($commit['parents'] as $parent){
+    if($count)
+    foreach($parents as $parent){
         echo "<div>".$parent."</div>";
     }
     echo "</td>";
